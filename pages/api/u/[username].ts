7b2dc42d44cf75ff45467f76fa3bf916b620c1d8
aldhanekaa <aldhanekadev@gmail.com>
2021-11-06 18:53:30 +0700

    okay

--- pages/api/u/[username].ts
+++ pages/api/u/[username].ts
@@ -6,46 +6,25 @@ import User, { UserInterface } from "models/User/User.model";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method == "GET") {
     await dbConncect();
-    const user = await User.findOne({ username: req.query.username });
+    const user = await User.findOne({ username: req.query.username }).select(
+      "-password"
+    );
     if (user) {
-      // @ts-ignore
-      const copyOfUser: UserInterface & { password: never; _id: string } = {
-        username: user.username,
-        _id: user._id,
-        isAccountVerified: user.isAccountVerified,
-        active: user.active, // shows if the student is alumni or not
-
-        fullName: user.fullName,
-        email: user.email,
-        avatar: user.avatar,
-
-        banner: user.banner,
-        bio: user.bio,
-        status: user.status,
-
-        gender: user.gender,
-
-        roles: user.roles,
-
-        roleInTechnoNatura: user.roleInTechnoNatura,
-
-        alumni: user.alumni,
-      };
       const userProject = await UserProjectModel.findOne({ userId: user._id });
-      console.log(userProject);
 
       if (!userProject) {
         res.send({
           message: "User found!",
           status: "success",
-          user: copyOfUser,
+          user: user,
         });
+        return;
       }
       res.send({
         message: "User project found!",
         status: "success",
         userProject: userProject,
-        user: copyOfUser,
+        user: user,
       });
       return;
     }
@@ -53,7 +32,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
-  res.send({});
+  res.send("yow!");
 };
 
 export default handler;
